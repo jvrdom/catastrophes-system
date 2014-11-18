6ad28aba8c5c65761af12a00de1e946625a49a75
nicocastro <nicoc.2002@gmail.com>
2014-11-18 00:21:12 -0300

    Se agrega metodo alta de usuario en WS

--- catastrophes-system-web/src/org/jboss/samples/rs/webservices/UsuarioResource.java
+++ catastrophes-system-web/src/org/jboss/samples/rs/webservices/UsuarioResource.java
@@ -1,20 +1,49 @@
 package org.jboss.samples.rs.webservices;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import com.ssacn.ejb.business.local.UserManager;
+import com.ssacn.ejb.business.remote.UserManagerRemote;
+
 @Path("/ServicesUsuario")
 public class UsuarioResource {
 	
-	@Path("/alta/{user}/{pass}")
+	private UserManagerRemote userM;
+	
+	@Path("/alta/{nombre}/{apellido}/{email}/{fechaNac}/{sexo}/{tel}/{pass}")
 	@POST
 	@Produces("application/json")
-	public String altaUsuario(@PathParam("user") String user, @PathParam("pass") String pass){
-		String result = "User : " + user + " Password : " + pass; 
-		return result;
+	public String altaUsuario(@PathParam("nombre") String nombre,
+			@PathParam("apellido") String ape,
+			@PathParam("email") String email,	
+			@PathParam("fechaNac") String fechaNac,
+			@PathParam("sexo") String sexo,			
+			@PathParam("tel") String tel,
+			@PathParam("pass") String pass) {
+		try {
+			
+			userM=new UserManager();
+			
+			SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+			Date fecha = formatter.parse(fechaNac);	
+			DateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+			String f=formato.format(fecha);
+			System.out.println(nombre+" "+ape+" "+email+" "+pass+" "+formato.parse(f)+" "+sexo);
+			userM.createUser(nombre, ape, email, pass, formato.parse(f), sexo);
+			return "ok";
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return ex.getMessage();
+		}
+		
 	}
 	
 	@GET
